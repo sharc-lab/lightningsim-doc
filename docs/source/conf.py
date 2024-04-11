2cff2879829fb0ffd61c35466b158eb1e76d713c
Callie-GT <81524274+Callie-GT@users.noreply.github.com>
2024-04-11 10:45:28 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LightningSim'
+copyright = '2024, Georgia Tech'
+author = 'Rishov Sarkar'
 
-release = '0.1'
-version = '0.1.0'
+release = '2.0'
+version = '2.0'
 
 # -- General configuration
 
